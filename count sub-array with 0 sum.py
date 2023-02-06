2243e7adf7b8b8c5064cab7642893e01cdf6bf96
Kunal Mahajan <78916226+kunal29july@users.noreply.github.com>
2023-02-06 23:22:03 +0530

    Create count sub-array with 0 sum.py

---
+++ count sub-array with 0 sum.py
@@ -0,0 +1,17 @@
+'''
+Input:  arr = [6, 3, -1, -3, 4, -2, 2, 4, 6, -12, -7]
+Output:  
+Subarray found from Index 2 to 4
+Subarray found from Index 2 to 6          
+Subarray found from Index 5 to 6
+Subarray found from Index 6 to 9
+Subarray found from Index 0 to 10
+
+
+
+approch :
+1. find prefix sum
+
+after that it is same question as below:
+2. count number of duplicate pairs using : freq(freq-1)//2
+'''
